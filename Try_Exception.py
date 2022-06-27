30f3c7207740458742fd535f0296ffdc1317825f
Lohith G K <86795049+Lohithgk@users.noreply.github.com>
2022-06-27 12:16:01 +0530

    Add files via upload

---
+++ Try_Exception.py
@@ -0,0 +1,15 @@
+
+# Exception handling in python..
+
+try:
+    print("This block has main Piece of code") # Main block
+
+except:
+    print("something is wrong") # This block will Execute if the Main block fails.
+
+else:
+    print("main block executed successfully") # This block will Execute if the Main block is successful.
+
+finally:
+    print("final block") # This block will execute if the main block is successful or not.
+
